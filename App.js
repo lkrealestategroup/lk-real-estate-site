6f78bb2b9d533c473403da2217cfb19ab7873d14
lkrealestategroup <kakkargarv72@gmail.com>
2025-05-23 16:08:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to LK Real Estate Group</h1>
+      <p>Serving Hisar, Haryana with 20+ years of experience.</p>
+    </div>
+  );
+}
